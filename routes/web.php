dc8b81fa47c3d07c89f653a97f64a8001bed3454
DeanFebrio <febriodean@gmail.com>
2026-05-20 14:39:41 +0700

    fix: routing

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\MembershipController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MembershipController;
 
 Route::get('/', function () {
     return view('welcome');
