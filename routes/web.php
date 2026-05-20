7e705e4e8286abb6fb7b956e5422fff6f7e6bfcb
DeanFebrio <febriodean@gmail.com>
2026-05-20 14:36:52 +0700

    fix: routing

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,3 @@ use App\Http\Controllers\MembershipController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::post('/api/membership', [MembershipController::class, 'register']);
